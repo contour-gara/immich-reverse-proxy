063928c83fa2e081b283a61c7a9546e9e20e7601
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-11-10 22:36:13 +0900

    IP アドレスの環境変数の修正

--- reflect_envs.sh
+++ reflect_envs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-envsubst '$IMMICH_ADDRESS' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$MY_GLOBAL_IP' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 echo `cat /etc/nginx/conf.d/default.conf`
